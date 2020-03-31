42fcbbe21f9a33de28a48ed1e4086316b168b03b
Jarryd <info.wiped@gmail.com>
2020-03-31 13:13:42 +0200

    Initial Commit

---
+++ website/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.home, name="home"),
+	path('contact.html', views.contact, name="contact"),
+	path('about.html', views.about, name="about"),
+	path('pricing.html', views.pricing, name="pricing"),
+	path('service.html', views.service, name="service"),
+	path('blog.html', views.blog, name="blog"),
+	path('blog_details.html', views.blog_details, name="blog_details"),
+	path('booking.html', views.booking, name="booking"),
+
+]
+
---
+++ website/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+	return render(request, 'home.html', {})
+
+def contact(request):
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		# send an email
+		send_mail(
+			message_name, # subject
+			message, # message
+			message_email, # from email
+			['info.wiped@gmail.com', 'jarryd.strydom@icloud.com'], # to Email
+			)
+
+		return render(request, 'contact.html', {'message_name': message_name})
+
+
+	else:
+		return render(request, 'contact.html', {})
+
+def about(request):
+	return render(request, 'about.html', {})
+
+def pricing(request):
+	return render(request, 'pricing.html', {})
+
+def service(request):
+	return render(request, 'service.html', {})
+
+def blog(request):
+	return render(request, 'blog.html', {})
+
+def blog_details(request):
+	return render(request, 'blog_details.html', {})
+
+
+def booking(request):
+	if request.method == "POST":
+		your_name = request.POST['your-name']
+		your_phone = request.POST['your-phone']
+		your_email = request.POST['your-email']
+		your_address = request.POST['your-address']
+		your_time = request.POST['your-time']
+		your_date = request.POST['your-date']
+		your_message = request.POST['your-message']
+
+		# send an email
+		booking = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: " + your_address + " Date: " + your_date + " Time: " + your_time + " Message: " + your_message
+		send_mail(
+			'Booking Request', # subject
+			booking, # message
+			your_email, # from email
+			message, #message
+			your_email # from email
+			['info@wiped.com', 'jarryd.strydom@icloud.com'], # To Email
+			)
+
+		return render(request, 'booking.html', {
+			'your_name': your_name,
+			'your_phone': your_phone,
+			'your_email': your_email,
+			'your_address': your_address,
+			'your_time': your_time,
+			'your_date': your_date,
+			'your_message': your_message
+			})
+
+
+	else:
+		return render(request, 'home.html', {})
+
+
+
+
+
+
